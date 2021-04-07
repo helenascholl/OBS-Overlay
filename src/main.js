8b93a6e66672a76f68aef57cf318d8edea03e400
schollsebastian <scholl.sebastian@gmx.at>
2021-04-07 21:14:50 +0200

    Implement main.js

--- src/main.js
+++ src/main.js
@@ -1,20 +1,16 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
 
-function createWindow() {
-  const window = new BrowserWindow({
-    width: 800,
-    height: 600
-  });
+const editorHtmlFile = 'editor/index.html';
+const overlayHtmlFile = 'overlay/index.html';
 
-  window.loadFile('index.html');
-}
+let editorWindow;
+let overlayWindow;
 
 app.whenReady().then(() => {
-  createWindow();
-
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
+  editorWindow = createWindow(editorHtmlFile, {
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
     }
   });
 
@@ -23,4 +19,48 @@ app.whenReady().then(() => {
       app.quit();
     }
   });
+
+  editorWindow.on('closed', () => {
+    if (overlayWindow) {
+      overlayWindow.close();
+    }
+  });
 });
+
+ipcMain.handle('reload-overlay', () => {
+  if (overlayWindow) {
+    overlayWindow.reload();
+  }
+});
+
+ipcMain.handle('open-overlay', () => {
+  createOverlayWindow();
+});
+
+function createWindow(htmlFile, config) {
+  const window = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    autoHideMenuBar: true,
+    backgroundColor: config && config.backgroundColor ? config.backgroundColor : 'white',
+    webPreferences: config && config.webPreferences ? config.webPreferences : {}
+  });
+
+  window.loadFile(htmlFile);
+
+  return window;
+}
+
+function createOverlayWindow() {
+  if (overlayWindow) {
+    overlayWindow.close();
+  }
+
+  overlayWindow = createWindow(overlayHtmlFile, {
+    backgroundColor: '#35BD09'
+  });
+
+  overlayWindow.on('close', () => {
+    overlayWindow = null;
+  });
+}
